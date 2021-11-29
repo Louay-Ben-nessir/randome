f75336282971223cff80c943ab2bfd397d87dd5b
Louay-Ben-nessir <91753931+Louay-Ben-nessir@users.noreply.github.com>
2021-11-29 22:59:37 +0100

    Update SORTING.c

--- SORTING.c
+++ SORTING.c
@@ -4,9 +4,15 @@
 #include <strings.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 
-
-
+// global decleration of the varuable so we can use it in all functions without passing it 
+const int  arg_len=100;
+int values_to_sort[256]; 
+int nbr_rect;
+int changed_values[1000][3] ;//first & second boxes to heighlight then 0 or 1 to swap or not ; and the forth is a special box that we want to bring attention to
+pthread_t Sorting_Thread;
+//----------------------------------------------------------------------------------------------------
 
 void sorting_types_formating(char *string, char **sorting_list, int nbr_sorting_types){
     char temp[15];
@@ -52,56 +58,44 @@ bool is_sorted_asscending(int input[],int size){
     for(int i = 0 ;i<size-1 ; i++) if (input[i]>input[i+1]) return false;
     return true;
 }
-void swap(int input[],int changes[],int sorting_algo){
-    if (changes[2]){
-    int aux=input[changes[0]];
-    input[changes[0]]=input[changes[1]];
-    input[changes[1]]=aux;
+void swap(int changes[]){
+    if (changes[0]){
+    int aux=values_to_sort[changes[1]];
+    values_to_sort[changes[1]]=values_to_sort[changes[2]];
+    values_to_sort[changes[2]]=aux;
     }
 }
-void Bogosort(int input[],int size,int *itr_nbr,int changes[]){
-    int current_position=*itr_nbr%size;
-    int r=rand()%size, swap = input[current_position];
-    while (r==current_position) r=rand()%size;
-    changes[0]=current_position;changes[1]=r;changes[2]=true;
-}
-void Bubble(int input[],int size,int *itr_nbr,int changes[]){
-    int current_position=*itr_nbr+1,nbr_cycles=1;
-    while(current_position>=size-nbr_cycles) {current_position-=size-nbr_cycles;nbr_cycles++;};   
-    changes[0]=current_position-1;changes[1]=current_position;changes[2]=input[changes[0]]>input[changes[1]];
-}
-void Selection(int input[],int size,int *itr_nbr,int changes[]){
-    if(changes[3]==-1) changes[3]=0; // the min tracking box should start at 0 if it dosent have any value befor hand 
-    int current_position=*itr_nbr+1,nbr_cycles=0;
-    while(current_position>=size-nbr_cycles) {current_position-=size-nbr_cycles-1;nbr_cycles++;};  
-    current_position+=nbr_cycles;
-    changes[0]=nbr_cycles;//of the first unsorted element
-    changes[1]=current_position;//normal counter going thorough every position
-    changes[2]=false;//swap at the start of every cycel 
-    changes[3]=(input[changes[3]]>input[changes[1]])?changes[1]:changes[3];
-    if(current_position-nbr_cycles==0){//finesed a loop and about to start another
-        changes[1]=changes[3];// take the smallest elemnt 
-        changes[2]=true;
-        changes[3]=nbr_cycles;
-        *itr_nbr-=1;//set back the itterations by one so the swap dosent count as a step
-    }
-   
-     
+void *Bogosort(){
+        int r=0;
+        int change_counter=0;
+        while (!(is_sorted_asscending(values_to_sort,nbr_rect)) && change_counter<1000){
+        for ( int i = 0 ; i< nbr_rect ; i++){
+            r=rand()%nbr_rect;
+            while (r==i) r=rand()%nbr_rect;
+            changed_values[change_counter][0]=1;
+            changed_values[change_counter][1]=i;
+            changed_values[change_counter][2]=r;  
+            change_counter++;
+        }
+        }
     
-    printf("%d\n",current_position-nbr_cycles);
-     
-    
+}
+void Bubble(){
+}
+void Selection(){
 }
 
-bool sort(int input[],int size , int sorting_algo, int *itr_nmb,int changes[2]){
-    if(!is_sorted_asscending(input,size)){ // 1 setp of any giving sorting algorithem
+bool sort(int sorting_algo){
+    if(!is_sorted_asscending(values_to_sort,nbr_rect)){ // 1 setp of any giving sorting algorithem
+        
         switch (sorting_algo)
         {
-        case 0: Bogosort(input,size,itr_nmb,changes);
+        case 0: pthread_create(&Sorting_Thread, NULL, Bogosort, NULL);
+                pthread_join(Sorting_Thread, NULL);
+                break;
+        case 1: Bubble();
             break;
-        case 1: Bubble(input,size,itr_nmb,changes);
-            break;
-        case 2: Selection(input,size,itr_nmb,changes);
+        case 2: Selection();
             break;
         default:
             break;
@@ -132,7 +126,7 @@ int main(){
     char input_values[256] = "1,2,3,4";//"Input the values you want sorted seperated by a ',' ";
     bool nmb_of_vals_f = false;//foucues on nmb value text box values 
     char nmb_of_vals[5] = "10";
-    int values_to_sort[256];
+
 
 
     // SORTING TYPE BTNS SELECT CONFIG AND VARS
@@ -147,15 +141,14 @@ int main(){
     //NON-GUI VARIABLES
     // ---------------------------------------------------------------------------
     int nbr_steps = -1; // the number of stepes to take in any giving sorting algo 
-    int nbr_rect = parse_input(input_values,&values_to_sort,256);
+    nbr_rect = parse_input(input_values,&values_to_sort,256);
     bool sorting = false;
-    int itr_nbr = 0;
     int delay=1000*(CLOCKS_PER_SEC/1000);
     clock_t time=clock()-delay;
-    int changed_values[3] = {-1, -1, 0, -1};//first & second boxes to heighlight then 0 or 1 to swap or not ; and the forth is a special box that we want to bring attention to
     Color rect_colour = MAROON;
     bool havent_swaped=false;
     bool Done_sorting=false;
+    int itr_nbr=0;
 
     // Main loop
     // ---------------------------------------------------------------------------
@@ -174,7 +167,7 @@ int main(){
         if (GuiTextBox((Rectangle){screenWidth-190,135,75,30},nmb_of_vals,5,nmb_of_vals_f)) nmb_of_vals_f=!nmb_of_vals_f;//
         if (GuiButton((Rectangle){screenWidth-110, 135, 50, 30 },"random")) nbr_rect=fill_rand(nmb_of_vals,input_values);//fill the array box with n = num_array random values after clearing it
         //place holder btns
-        if (GuiButton((Rectangle){screenWidth-110, 200, 50, 30 },"SORT")) {nbr_steps = -1;sorting=true;}
+        if (GuiButton((Rectangle){screenWidth-110, 200, 50, 30 },"SORT")) {nbr_steps = -1;sorting=true;sort(sorting_type_selector);}
         if (GuiButton((Rectangle){screenWidth-110, 300, 50, 30 },"STOP SORT")) {nbr_steps = 0;sorting=false;} 
 
         //draw the rectangels 
@@ -182,22 +175,23 @@ int main(){
         if (!sorting){
             nbr_rect=parse_input(input_values,&values_to_sort,256);// keep parsing the input as long as the text box is foucesed and not currently sorting
             rescale_input(&values_to_sort, nbr_rect);
-            itr_nbr=0;
-            changed_values[0]=-1;changed_values[1]=-1;changed_values[2]=false,changed_values[3]=-1;
+            
+            
         }else if(clock()>time+delay) {
-            Done_sorting=sort(values_to_sort,nbr_rect,sorting_type_selector,&itr_nbr,changed_values);
+            itr_nbr++;
             time=clock();
-            itr_nbr+=1;//position cant be bigger than nbr rect
             havent_swaped=true;
+            Done_sorting=is_sorted_asscending(values_to_sort,nbr_rect);
             }
         if (Done_sorting){
             //whatere we want to go after we finish sorting goes here 
             havent_swaped=false;
         }
-        if(havent_swaped && clock()>=time+(delay/2)) {swap(values_to_sort,changed_values,sorting_type_selector);havent_swaped=false;} // at hafe of the delay time switch the boxes so it's heighlited first then switched
+        if(havent_swaped && clock()>=time+(delay/2)) {swap(changed_values[itr_nbr]);havent_swaped=false;} // at hafe of the delay time switch the boxes so it's heighlited first then switched
+        
         for (int i = 0 ; i<nbr_rect;i++){ //draw the inital rectangels   
-            if (i==changed_values[0] || i==changed_values[1]) rect_colour = BLACK; // HEIGHTLIGHT THE CHANGEING BLOCKS
-            if (i==changed_values[3]) rect_colour = YELLOW;
+            if (i==changed_values[itr_nbr][1] || i==changed_values[itr_nbr][2]) rect_colour = BLACK; // HEIGHTLIGHT THE CHANGEING BLOCKS
+
             DrawRectangle(200+i*((screenWidth-485)/nbr_rect), screenHeight-25-values_to_sort[i], (screenWidth-490)/nbr_rect, values_to_sort[i],rect_colour);//last rect width +5 white pixel seperator   485 490              
             rect_colour= MAROON;
             }
